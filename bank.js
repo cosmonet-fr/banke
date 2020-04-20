ae7040c1705ef774d22e6e79b3778fca81d91dc9
cosmonet-fr <60877684+cosmonet-fr@users.noreply.github.com>
2020-04-20 09:51:11 +0200

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,46 @@
+class BankAccount {
+  constructor (user, money, overdraft){
+    this.user = user;
+    this.money = money;
+    this.overdraft = overdraft;
+  }
+   // Ajout d'une méthode
+  currentCash() {
+    console.log('Le solde de ' + this.user + 'est de ' + this.money + ' BitCointCash.');
+  }
+  addCash(sumDeposited) {
+    
+    if ( sumDeposited > 0 ) {
+      this.money = this.money + sumDeposited;
+      console.log('Le nouveau solde de ' + this.user + ' est de ' + this.money + ' BitCointCash après ce dépot.');
+      } else {
+        console.log('Erreur ! Un dépot négatif ou null est interdit !');
+      }
+    }
+  removCash(sumRemoved) {
+    if (sumRemoved > 0) {
+      let newMoney = this.money - sumRemoved;
+      if (newMoney >= this.overdraft){
+        this.money = newMoney;
+        console.log('Le nouveau solde de ' + this.user + ' est de ' + this.money + ' BitCointCash après ce retrait.')
+      } else {
+        console.log('Le découvert autorisé de ' + this.user + ' ne permet pas ce retrait.');
+     }
+      
+    } else {
+      console.log('Erreur ! Un retrait négatif ou null est interdit !');
+    }
+  }
+}
+
+const picsouAccount = new BankAccount (
+  "Picsou",
+  76098,
+  -700);
+
+console.log(picsouAccount.currentCash());
+
+console.log(picsouAccount.addCash(500));
+
+console.log(picsouAccount.removCash(77298));
+
